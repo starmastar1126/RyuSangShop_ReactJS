a578a3f56d773389a1f248b6649165bea106dc8b
starmastar1126 <starmastar1126@gmail.com>
2025-06-06 10:14:43 -0700

    completed category

---
+++ src/store/basic/category/index.js
@@ -0,0 +1,154 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+import toast from 'react-hot-toast'
+
+// ** Config
+import { Http } from 'src/configs/http'
+import urls from 'src/configs/urls'
+import utils from 'src/configs/utils'
+
+// ** Fetch Categorys
+export const fetchCategory = createAsyncThunk('category/fetchData', async (params, thunkAPI) => {
+  const header = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${window.localStorage.getItem(urls.access)}`
+  }
+
+  try {
+    let url = utils.toURL({
+      url: urls.category,
+      query: params
+    })
+
+    let response = await Http.get(url, header)
+    const { status, data } = response
+    if (status === 200) {
+      return data.data
+    } else {
+      return thunkAPI.rejectWithValue()
+    }
+  } catch (error) {
+    return thunkAPI.rejectWithValue()
+  }
+})
+
+export const addCategory = createAsyncThunk('category/addData', async (params, thunkAPI) => {
+  const header = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${window.localStorage.getItem(urls.access)}`
+  }
+
+  try {
+    let url = utils.toURL({
+      url: urls.category,
+      query: params
+    })
+
+    let response = await Http.post(url, params, header)
+    const { status, data } = response
+    if (status === 200 || status === 201) {
+      toast.success('Successfully added!')
+      return data.data
+    } else {
+      toast.error('Failure adding.')
+      return thunkAPI.rejectWithValue()
+    }
+  } catch (error) {
+    if (error.response && error.response.status === 409) {
+      toast.error('Already exist.')
+    } else {
+      toast.error(error.message)
+    }
+    return thunkAPI.rejectWithValue()
+  }
+})
+
+export const editCategory = createAsyncThunk('category/editData', async (params, thunkAPI) => {
+  const header = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${window.localStorage.getItem(urls.access)}`
+  }
+
+  try {
+    let url = utils.toURL({
+      url: urls.category,
+      query: params
+    })
+
+    let response = await Http.put(url, params, header)
+    const { status, data } = response
+    if (status === 200) {
+      toast.success('Successfully saved!')
+      return data.data
+    } else {
+      toast.error('Failure saving.')
+      return thunkAPI.rejectWithValue()
+    }
+  } catch (error) {
+    if (error.response && error.response.status === 409) {
+      toast.error('Already exist.')
+    } else {
+      toast.error(error.message)
+    }
+    return thunkAPI.rejectWithValue()
+  }
+})
+
+export const deleteCategory = createAsyncThunk('category/deleteData', async (params, thunkAPI) => {
+  const header = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${window.localStorage.getItem(urls.access)}`
+  }
+
+  try {
+    let url = utils.toURL({
+      url: urls.category,
+      query: params
+    })
+
+    let response = await Http.delete(url, header)
+    const { status, data } = response
+    if (status === 200) {
+      toast.success('Successfully deleted!')
+      return data.data
+    } else if (status === 204) {
+      toast.success('Successfully deleted!')
+      return { items: [], total: 0 }
+    } else {
+      toast.error('Failure deleting.')
+      return thunkAPI.rejectWithValue()
+    }
+  } catch (error) {
+    return thunkAPI.rejectWithValue()
+  }
+})
+
+export const categorySlice = createSlice({
+  name: 'category',
+  initialState: {
+    data: [],
+    total: 0
+  },
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(fetchCategory.fulfilled, (state, action) => {
+        state.data = action.payload?.items
+        state.total = action.payload?.total
+      })
+      .addCase(addCategory.fulfilled, (state, action) => {
+        state.data = action.payload?.items
+        state.total = action.payload?.total
+      })
+      .addCase(editCategory.fulfilled, (state, action) => {
+        state.data = action.payload?.items
+        state.total = action.payload?.total
+      })
+      .addCase(deleteCategory.fulfilled, (state, action) => {
+        state.data = action.payload?.items
+        state.total = action.payload?.total
+      })
+  }
+})
+
+export default categorySlice.reducer
